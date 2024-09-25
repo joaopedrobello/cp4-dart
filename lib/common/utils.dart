154883bd62468409c695aec994e0db0a50d8bdc5
João Pedro Bello <57834603+joaopedrobello@users.noreply.github.com>
2024-09-24 23:53:11 -0300

    tirando apiKey

--- lib/common/utils.dart
+++ lib/common/utils.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 const imageUrl = 'https://image.tmdb.org/t/p/w500';
 const kBackgoundColor = Color(0xff121012);
 const kbuttonColor = Color.fromARGB(255, 89, 54, 133);
-const apiKey = 'd91893342be015590d0cc4738f5a1829';
+const apiKey = '';
 Color backgroundPrimary = Color.fromARGB(255, 171, 169, 180);
 
 String getGenres(List<int> genres) {
